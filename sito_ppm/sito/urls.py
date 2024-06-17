95bf0b50f4ea804c0b05f305603d066bf9c72216
mariavittoria <mvcollini@gmail.com>
2024-06-17 22:14:19 +0200

    recipe display, delete and modify(to be fixed)

--- sito_ppm/sito/urls.py
+++ sito_ppm/sito/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import (homepage, home, mylogin, signin, registra, esci, login_view,profilo,
-                    mysetting,impostazioni,crea, create_recipe )
+from .views import (homepage, home, mylogin, signin, registra, esci, login_view, profilo,
+                    mysetting, impostazioni, crea, create_recipe, recipe_detail, delete_recipe,
+                    modifica_ricetta, update_ricetta)
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -13,10 +14,14 @@ urlpatterns = [
     path("esci/", esci, name="esci"),
     path("login_view/", login_view, name="login_view"),
     path("profilo/", profilo, name='profilo'),
-    path("mysetting/",mysetting, name='mysetting'),
+    path("mysetting/", mysetting, name='mysetting'),
     path("impostazioni/", impostazioni, name="impostazioni"),
-    path("crea/",crea, name='crea'),
-    path('create/', create_recipe, name='create_recipe')
+    path("crea/", crea, name='crea'),
+    path('create/', create_recipe, name='create_recipe'),
+    path('recipe/<int:id>/', recipe_detail, name='recipe_detail'),
+    path('recipe/<int:recipe_id>/delete/', delete_recipe, name='delete_recipe'),
+    path('recipe/<int:id>/modifica', modifica_ricetta, name='modifica_ricetta'),
+    path("modificaricetta/<int:id>/", update_ricetta, name='update_ricetta')
 
 ]
 
--- sito_ppm/sito/views.py
+++ sito_ppm/sito/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -20,12 +20,14 @@ def signin(request):
     return render(request, 'Registrazione.html')
 
 
+# da eliminare
 def home(request):
     return render(request, 'prova2.html')
 
 
 def profilo(request):
-    return render(request, 'profile.html')
+    recipes = Recipe.objects.filter(user=request.user)
+    return render(request, 'profile.html', {'recipes': recipes})
 
 
 def mysetting(request):
@@ -85,11 +87,13 @@ def login_view(request):
     return render(request, 'login.html')
 
 
+@login_required
 def esci(request):
     logout(request)
     return redirect('mylogin')
 
 
+@login_required
 def impostazioni(request):
     if request.method == 'POST':
 
@@ -178,3 +182,70 @@ def create_recipe(request):
         return redirect('profilo')
     categories = Category.objects.all()
     return render(request, 'ricette.html', {'categories': categories})
+
+
+@login_required
+def recipe_detail(request, id):
+    recipe = get_object_or_404(Recipe, id=id)
+    return render(request, 'recipe_detail.html', {'recipe': recipe})
+
+
+@login_required
+def delete_recipe(request, recipe_id):
+    recipe = get_object_or_404(Recipe, id=recipe_id, user=request.user)
+    if request.method == 'POST':
+        recipe.delete()
+        return redirect('profilo')
+    return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
+
+
+@login_required
+def modifica_ricetta(request, id):
+    recipe = get_object_or_404(Recipe, id=id)
+    return render(request, 'modifica_ricetta.html', {'recipe': recipe})
+
+
+@login_required
+def update_ricetta(request, recipe_id):
+    recipe = get_object_or_404(Recipe, id=recipe_id, user=request.user)
+    if request.method == 'POST':
+        titolo = request.POST.get('titolo')
+        ingredienti = request.POST.get('ingredienti')
+        foto = request.POST.get('foto')
+        istruzioni = request.POST.get('istruzioni')
+        tempo = request.POST.get('tempo')
+        categoria = request.POST.get('category')
+
+        if not titolo or not categoria or not ingredienti or not istruzioni or not tempo or not foto:
+            messages.error(request, 'Non ci sono stati cambiamenti')
+            return redirect('modifica_ricetta', {'recipe': recipe})
+        if not titolo:
+            titolo = recipe.title
+        if not ingredienti:
+            ingredienti = recipe.ingredients
+        if not foto:
+            foto = recipe.photo
+        if not istruzioni:
+            istruzioni = recipe.instructions
+        if not tempo:
+            tempo = recipe.preparationtime
+
+        try:
+            category = Category.objects.get(id=categoria)
+        except Category.DoesNotExist:
+            messages.error(request, 'Invalid category.')
+            return redirect('create_recipe')
+
+        recipe = Recipe(
+            title=titolo,
+            photo=foto,
+            preparationtime=tempo,
+            category=category,
+            user=request.user,
+            ingredients=ingredienti,
+            instructions=istruzioni
+        )
+        recipe.save()
+        return redirect('profilo')
+        categories = Category.objects.all()
+        return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
